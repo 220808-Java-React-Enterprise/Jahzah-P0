ca4010574615e85418a137ef377573d80c51d702
Jahzah <c00227447@louisiana.edu>
2022-08-23 12:05:45 -0500

    adding class for the location of inventory

---
+++ src/main/java/com/revature/metallicgems/models/Location.java
@@ -0,0 +1,70 @@
+package com.revature.metallicgems.models;
+
+public class Location {
+    private String id;
+    private String street;
+    private String name;
+    private String city;
+    private String state;
+    private long zipcode;
+
+    public Location() {
+    }
+
+    public Location(String id, String street, String name, String city, String state, long zipcode){
+        this.id = id;
+        this.street = street;
+        this.name = name;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public long getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(long zipcode) {
+        this.zipcode = zipcode;
+    }
+}
